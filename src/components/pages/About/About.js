8c02221681a64178121a83205e93bcc74a975b50
Davron <87236055+Davronboy@users.noreply.github.com>
2021-10-20 09:51:17 +0300

    Update About.js

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -2,7 +2,6 @@ import React from "react";
 import AboutSec1 from "../../containers/about/Section 1";
 import AboutSec2 from "../../containers/about/Section 2";
 import AboutSec3 from "../../containers/about/Section 3";
-import { BrowserHistory } from "react-router-dom";
 const About = () => {
   return (
     <div>
